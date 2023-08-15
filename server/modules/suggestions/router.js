ede43f1ce46bbec4e58e57f8783b48fa92ab31c6
dev-hayan <abdul.hayan@devsinc.com>
2023-08-15 16:02:48 +0500

    Changes are done told by buddy

--- server/modules/suggestions/router.js
+++ server/modules/suggestions/router.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const { createSuggestion, getPostSuggestions, getUserSuggestions, getUserPostSuggestion } = require('./suggestions_controller')
+const { createSuggestion, getPostSuggestions, getUserSuggestions, getUserPostSuggestion, rejcetSuggestion } = require('./suggestions_controller')
 const router = express.Router()
 
 router.post("/", createSuggestion)
 router.get("/post/:id", getPostSuggestions)
 router.get("/userSuggestions/:id", getUserSuggestions)
 router.get("/userPostSuggestions/:id/", getUserPostSuggestion)
+router.patch("/reject/:id", rejcetSuggestion);
 module.exports = router
